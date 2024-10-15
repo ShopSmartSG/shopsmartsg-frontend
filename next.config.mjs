f831fc5f21f24831048f33c3cb514c402a4424cc
Aakash Darsi <darsisriguruaakash@gmail.com>
2024-10-15 17:45:24 +0800

    Environment Variables

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   images: {
     domains: ["via.placeholder.com"],
-  },
-  env: {
-    GOOGLE_MAPS_API_KEY: "AIzaSyC4CMPKutQEt59q-1OCRsOdnAUxJetn7GQ",
-  },
+  }
 };
 
 export default nextConfig;
