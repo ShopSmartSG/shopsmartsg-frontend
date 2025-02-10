81bdf5531aaf605e007e074b85065a212e1dc58c
Aakash Darsi <darsisriguruaakash@gmail.com>
2025-02-10 20:23:27 +0800

    Responsive For the Customer Register Page

--- src/app/customer/register/page.tsx
+++ src/app/customer/register/page.tsx
@@ -9,23 +9,25 @@ import { Toast } from "primereact/toast";
 import { Card } from "primereact/card";
 import { Message } from "primereact/message";
 import { InputOtp } from "primereact/inputotp";
+import { Grid, Row, Col } from "primereact/grid";
 import "./login.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+
 const EmailOtpForm = () => {
   const [email, setEmail] = useState("");
   const [otp, setOtp] = useState("");
   const [name, setname] = useState("");
   const [addressLine1, setAddressLine1] = useState("");
   const [addressLine2, setAddressLine2] = useState("");
-  const [phoneNumber,setPhoneNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [otpError, setOtpError] = useState(false);
-  const [pinCode,setPinCode] = useState("");
+  const [pinCode, setPinCode] = useState("");
   const [showOtpDialog, setShowOtpDialog] = useState(false);
   const [emailError, setEmailError] = useState("");
   const [otpCount, setOtpCount] = useState(0);
   const [resendDisabled, setResendDisabled] = useState(true);
-  const [timer, setTimer] = useState(30); //
+  const [timer, setTimer] = useState(30);
   const disabledCondition: boolean = otpCount >= 3;
   const toast = useRef(null);
 
@@ -71,15 +73,16 @@ const EmailOtpForm = () => {
     }
   };
 
-
   const generateOtp = async () => {
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/generateOtp/customer`, {
-          email:email
-      }, {
-          withCredentials: true
-        }
+          `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/generateOtp/customer`,
+          {
+            email: email,
+          },
+          {
+            withCredentials: true,
+          }
       );
       if (response.status == 200) {
         toast.current.show({
@@ -91,44 +94,43 @@ const EmailOtpForm = () => {
         setOtpCount(otpCount + 1);
         setTimer(30);
       }
+    } catch (error) {
+      toast.current.show({});
     }
-    catch (error) {
-      toast.current.show({})
-  }
-  }
+  };
 
-   const handleOtpSubmit = async () => {
-     try {
-       const response = await axios.post(
-         `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/verifyOtp/customer`,
-         {
-           email: email,
-           emailAddress: email,
-           otp: otp,
-         },
-         { withCredentials: true }
-       );
+  const handleOtpSubmit = async () => {
+    try {
+      const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/verifyOtp/customer`,
+          {
+            email: email,
+            emailAddress: email,
+            otp: otp,
+          },
+          { withCredentials: true }
+      );
 
-       if (response.status) {
-         toast.current.show({
-           severity: "success",
-           summary: "Success",
-           detail: "Login Successful",
-           life: 3000,
-         });
-         setShowOtpDialog(false);
-         setTimeout(() => {
-           router.push("/merchant/manage/view");
-         }, 3000);
-       }
-     } catch (error) {
-       toast.current.show({
-         status: "error",
-         message: "Error submitting OTP. Please try again later.",
-       });
-       setShowOtpDialog(false);
-     }
-   };
+      if (response.status) {
+        toast.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: "Login Successful",
+          life: 3000,
+        });
+        setShowOtpDialog(false);
+        setTimeout(() => {
+          router.push("/merchant/manage/view");
+        }, 3000);
+      }
+    } catch (error) {
+      toast.current.show({
+        status: "error",
+        message: "Error submitting OTP. Please try again later.",
+      });
+      setShowOtpDialog(false);
+    }
+  };
 
   const handleResendOtp = () => {
     setResendDisabled(true);
@@ -141,22 +143,21 @@ const EmailOtpForm = () => {
     });
   };
 
-
   const customerRegister = async () => {
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/customer`,
-        {
-          email,
-          name,
-          addressLine1,
-          addressLine2,
-          phoneNumber,
-          pinCode,
-        },
-        {
-          withCredentials: true, // Include credentials with the request
-        }
+          `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/customer`,
+          {
+            email,
+            name,
+            addressLine1,
+            addressLine2,
+            phoneNumber,
+            pinCode,
+          },
+          {
+            withCredentials: true,
+          }
       );
 
       if (response.status == 200) {
@@ -168,8 +169,7 @@ const EmailOtpForm = () => {
         });
         setShowOtpDialog(true);
       }
-    }
-    catch (error) {
+    } catch (error) {
       toast.current.show({
         severity: "error",
         summary: "Error",
@@ -178,21 +178,22 @@ const EmailOtpForm = () => {
       });
       setShowOtpDialog(false);
     }
-  }
+  };
+
   const handleEmail = async () => {
     setShowOtpDialog(true);
     setResendDisabled(true);
 
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/verifyOtp/customer`,
-        {
-          email,
-          otp,
-        },
-        {
-          withCredentials: true, // Include credentials with the request
-        }
+          `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/register/verifyOtp/customer`,
+          {
+            email,
+            otp,
+          },
+          {
+            withCredentials: true,
+          }
       );
       if (response.status == 200) {
         toast.current.show({
@@ -201,7 +202,7 @@ const EmailOtpForm = () => {
           detail: "OTP Validated Successfully",
           life: 3000,
         });
-       customerRegister();
+        customerRegister();
       }
     } catch (error) {
       console.error("Error sending email: ", error);
@@ -214,134 +215,161 @@ const EmailOtpForm = () => {
     }
   };
 
-  
   return (
-    <div
-      className="flex-row justify-content-center flex-wrap "
-      style={{ height: "100vh" }}
-    >
-      <Card>
-        <p className="p-card-title text-center">Register Page</p>
-        <div className="flex align-items-center justify-content-center">
-          <form onSubmit={handleEmailSubmit} className="p-fluid">
-            <div className="field">
-              <label htmlFor="email">Email</label>
-              <InputText
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                onBlur={(e) => blurHandler(e.target.value)}
-                invalid={emailError == "" ? false : true}
-                placeholder="Please Enter your Valid Email"
-                style={{ width: "476px", display: "block" }}
-              />
-              {emailError && <small className="p-error">{emailError}</small>}
+      <div className="p-grid p-justify-center p-align-center" style={{ height: "100vh" }}>
+        <div className="p-col-12 p-md-8 p-lg-6">
+          <Card>
+            <p className="p-card-title text-center">Register Page</p>
+            <div className="p-fluid">
+              <form onSubmit={handleEmailSubmit}>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="email" className="p-col-12 p-md-3">
+                    Email
+                  </label>
+                  <div className="p-col-12 p-md-9">
+                    <InputText
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        onBlur={(e) => blurHandler(e.target.value)}
+                        invalid={emailError == "" ? false : true}
+                        placeholder="Please Enter your Valid Email"
+                    />
+                    {emailError && <small className="p-error">{emailError}</small>}
+                  </div>
+                </div>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="name" className="p-col-12 p-md-3">
+                    Name
+                  </label>
+                  <div className="p-col-12 p-md-9 mt-2">
+                    <InputText
+                        id="name"
+                        placeholder="Enter Your Name"
+                        value={name}
+                        onChange={(e) => setname(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="pincode" className="p-col-12 p-md-3">
+                    PinCode
+                  </label>
+                  <div className="p-col-12 p-md-9 mt-2">
+                    <InputText
+                        id="pincode"
+                        placeholder="Enter Your PinCode"
+                        value={pinCode}
+                        onChange={(e) => setPinCode(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="addressLine1" className="p-col-12 p-md-3">
+                    Address Line 1
+                  </label>
+                  <div className="p-col-12 p-md-9 mt-2">
+                    <InputText
+                        id="addressLine1"
+                        placeholder="Enter Your Address Line 1"
+                        onChange={(e) => setAddressLine1(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="addressLine2" className="p-col-12 p-md-3">
+                    Address Line 2
+                  </label>
+                  <div className="p-col-12 p-md-9">
+                    <InputText
+                        id="addressLine2"
+                        placeholder="Enter Your Address Line 2"
+                        onChange={(e) => setAddressLine2(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="p-field p-grid mt-2">
+                  <label htmlFor="phoneNumber" className="p-col-12 p-md-3">
+                    Phone Number
+                  </label>
+                  <div className="p-col-12 p-md-9 mt-2">
+                    <InputText
+                        id="phoneNumber"
+                        placeholder="Enter Your Phone Number"
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="p-field p-grid">
+                  <div className="mt-2">
+                    <Button
+                        label="Next"
+                        type="submit"
+                        disabled={emailError == "" ? false : true}
+                        onClick={generateOtp}
+                    />
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="field">
-              <label htmlFor="">Name</label>
-              <InputText
-                id="name"
-                placeholder="Enter Your Name"
-                value={name}
-                onChange={(e) => setname(e.target.value)}
-              />
-            </div>
-            <div className="field">
-              <label htmlFor="">PinCode</label>
-              <InputText
-                id="pincode"
-                placeholder="Enter Your PinCode"
-                value={pinCode}
-                onChange={(e) => setPinCode(e.target.value)}
-              />
-            </div>
-            <div className="field">
-              <label htmlFor="">Address Line 1</label>
-              <InputText
-                id="addressLine1"
-                placeholder="Enter Your Address Line 1"
-                onChange={(e) => setAddressLine1(e.target.value)}
-              />
-            </div>
-            <div className="field">
-              <label htmlFor="">Address Line 2</label>
-              <InputText
-                id="addressLine2"
-                placeholder="Enter Your Address Line 2"
-                onChange={(e) => setAddressLine2(e.target.value)}
-              />
-            </div>
-            <div className="field">
-              <label htmlFor="">Phone Number</label>
-              <InputText
-                id="phoneNumber"
-                placeholder="Enter Your Phone Number"
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-            </div>
+          </Card>
+        </div>
 
-            <div className="p-card-footer min-w-4">
+        <Dialog
+            header="Enter OTP"
+            visible={showOtpDialog}
+            onHide={() => setShowOtpDialog(false)}
+            resizable={false}
+            draggable={false}
+            modal={true}
+        >
+          <div className="p-field p-grid">
+            <label htmlFor="otp" className="p-col-12 p-md-3">
+              OTP
+            </label>
+            <div className="p-col-12 p-md-9">
+              <InputOtp
+                  id="otp"
+                  value={otp}
+                  onChange={(e) => setOtp(e.value.toString())}
+                  length={6}
+                  disabled={disabledCondition}
+              />
+              {otpCount >= 3 && (
+                  <Message
+                      severity="error"
+                      text="Maximum Exhaust reached Please try again later"
+                  />
+              )}
+            </div>
+          </div>
+          <div >
+            <div>
               <Button
-                label="Next"
-                type="submit"
-                style={{ width: "80px" }}
-                disabled={emailError == "" ? false : true}
-                onClick={generateOtp}
+                  label="Submit"
+                  onClick={handleEmail}
+                  disabled={disabledCondition}
               />
             </div>
-          </form>
-        </div>
-      </Card>
-
-      <Dialog
-        header="Enter OTP"
-        visible={showOtpDialog}
-        onHide={() => setShowOtpDialog(false)}
-        resizable={false}
-        draggable={false}
-        modal={true}
-      >
-        <div className="field">
-          <label htmlFor="otp">OTP</label>
-          <InputOtp
-            id="otp"
-            value={otp}
-            onChange={(e) => setOtp(e.value.toString())}
-            length={6}
-            disabled={disabledCondition}
-          />
-          {otpCount >= 3 && (
-            <Message
-              severity="error"
-              text="Maximum Exhaust reached Please try again later"
-            />
+            <div className="p-col-12 p-md-6">
+              <Button
+                  label={`Resend OTP ${resendDisabled ? `(${timer}s)` : ""}`}
+                  onClick={handleResendOtp}
+                  severity="secondary"
+                  disabled={resendDisabled || disabledCondition}
+              />
+            </div>
+          </div>
+          {!disabledCondition && (
+              <div className="p-grid">
+                <div className="p-col-12">
+                  <Message severity="info" text="OTP will expire in 30 seconds" />
+                </div>
+              </div>
           )}
-        </div>
-        <div className="flex flex-row flex-wrap">
-          <div>
-            <Button
-              label="Submit"
-              onClick={handleEmail}
-              disabled={disabledCondition}
-            />
-          </div>
-          <div className="ml-2">
-            <Button
-              label={`Resend OTP ${resendDisabled ? `(${timer}s)` : ""}`}
-              onClick={handleResendOtp}
-              severity="secondary"
-              disabled={resendDisabled || disabledCondition}
-            />
-          </div>
-        </div>
-        {!disabledCondition && (
-          <div className="text-right">
-            <Message severity="info" text="OTP will expire in 30 seconds" />
-          </div>
-        )}
-      </Dialog>
-      <Toast ref={toast} />
-    </div>
+        </Dialog>
+        <Toast ref={toast} />
+      </div>
   );
 };
 
