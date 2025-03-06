256024cf9766d9eb85a13d90c7992f00b1137bab
Aakash Darsi <darsisriguruaakash@gmail.com>
2025-03-06 16:46:39 +0800

    check

--- shared/components/login/Login.tsx
+++ shared/components/login/Login.tsx
@@ -111,7 +111,7 @@ const Login = ({type}: LoginProps) => {
         const userType = type === "Customer" ? "customer" : "merchant";
            try {
              const response = await axios.get(
-               `${process.env.NEXT_PUBLIC_CentralServiceLogin_API_URL}/profile/login/verifyOtp/${userType}`,
+               `${process.env.NEXT_PUBLIC_CentralService_API_URL}/auth/google/login/${userType}/`,
                { withCredentials: true }
              );
 
